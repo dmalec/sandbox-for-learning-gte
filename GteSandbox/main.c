da7e630107e2935f4a41d9415d4873e9dfaa4c16
Dan Malec <dan.malec@gmail.com>
2022-07-28 21:23:58 -0400

    Updated with code from Lucas Scharenbroich to handle allocating memory from bank 0 as well as using Tool160 as a user tool installed in the same directory as the application.

--- GteSandbox/main.c
+++ GteSandbox/main.c
@@ -1,112 +1,99 @@
+#include <loader.h>
 #include <locator.h>
 #include <memory.h>
 #include <misctool.h>
 #include <types.h>
 
-#include "main.h"
-#include "gte.h"
 #include "demo_data.h"
-
+#include "gte.h"
 
 #define TOOLFAIL(string) \
-    if (toolerror()) SysFailMgr(toolerror(), "\p" string "\n\r    Error Code -> $");
+  if (toolerror()) SysFailMgr(toolerror(), "\p" string "\n\r    Error Code -> $");
 
 #define LEFT_ARROW      0x0008
 #define RIGHT_ARROW     0x0015
 #define UP_ARROW        0x000B
 #define DOWN_ARROW      0x000A
 
+#define SPRITE_START_TILE 2
+#define SPRITE_SLOT 0
+#define SPRITE_VBUFF (GTE_VBUFF_SPRITE_START+0*GTE_VBUFF_SPRITE_STEP)
+
+
 int main (void) {
-  Ref toolStartupRef;
   Word controlMask;
   Word keyPress;
-  unsigned int userId;
-  Word playerSpriteDecriptor = GTE_SPRITE_8X8 + 2;
-  Word playerSpriteVbuff = GTE_VBUFF_SPRITE_START + 0 * GTE_VBUFF_SPRITE_STEP;
-  Word playerSpriteSlot = 0;
+  Word userId;
+  Handle dpHndl;
+  Word dpWord;
   Word playerX = 10;
   Word playerY = 20;
-  Word yDiff = 1;
-  Word xDiff = 1;
-  Word page;
-
-  userId = MMStartUp();
-  TOOLFAIL("Unable to start memory manager");
 
   TLStartUp();
   TOOLFAIL("Unable to start tool locator");
 
-  toolStartupRef = StartUpTools(userId, refIsResource, rez_tools);
-  TOOLFAIL("Unable to start tools");
+  userId = MMStartUp();
+  TOOLFAIL("Unable to start memory manager");
 
-  // NOTE: The first zero (for dPageAddr) seems incorrect based on looking at the
-  // demos which call thusly (adding 0x0100 to the direct page register value):
-  //
-  //   clc
-  //   tdc
-  //   adc   #$0100
-  //   pha
-  //   pea   #0
-  //   lda   MyUserId
-  //   pha
-  //   _GTEStartUp
-  //
-  // I am unsure how to do the equivalent in C. The value 0 does not cause
-  // immediate issues; but, I'm worried it's causing the stability issues over time.
-  
-  GTEStartUp(0x0000, 0x0000, userId);
+  MTStartUp();
+  TOOLFAIL("Unable to start misc tools");
+
+  LoadGTEToolSet(userId);
+
+  dpHndl = NewHandle(0x0200, userId, 0x4015, 0);
+  if (dpHndl == NULL) {
+    TOOLFAIL("Unable to allocate bank 0 memory");
+  }
+  dpWord = (Word)(*dpHndl);
+  if ((dpWord & 0x00FF) != 0x0000) {
+    TOOLFAIL("Allocated bank 0 memory is not aligned");
+  }
+
+  GTEStartUp(dpWord, 0x0000, userId);
   TOOLFAIL("Unable to start GTE");
 
-  GTESetScreenMode(320, 200);
-  
+  GTESetScreenMode(160, 200);
   GTELoadTileSet(tiles);
-
-  GTESetPalette(0, (Ptr)palette);
-
+  GTESetPalette(0, (Pointer)palette);
   GTEFillTileStore(1);
   GTESetBG0Origin(0, 0);
 
-  GTECreateSpriteStamp(playerSpriteDecriptor, playerSpriteVbuff);
-  GTEAddSprite(playerSpriteDecriptor, playerX, playerY, playerSpriteSlot);
-  GTEUpdateSprite(playerSpriteSlot, 0x0000, playerSpriteVbuff);
+  GTECreateSpriteStamp(GTE_SPRITE_8X8 | SPRITE_START_TILE, SPRITE_VBUFF);
+  GTEAddSprite(SPRITE_SLOT, 0x0000, SPRITE_VBUFF, playerX, playerY);
 
   do {
     controlMask = GTEReadControl();
     keyPress = controlMask & 0x007F;
-    
-    if (keyPress == LEFT_ARROW && playerX > 0) {
-      playerX = playerX - 1;
-    }
-    
-    // NOTE: This is another interesting point. I would expect this value to
-    // be 320 (width of screen) - 8 (width of sprite). I'm not sure what I'm
-    // doing wrong setting up widths to result in this being the boundary for
-    // the sprite lining up with the right side of the screen.
-    if (keyPress == RIGHT_ARROW && playerX < 156) {
-      playerX = playerX + 1;
-    }
 
-    if (keyPress == UP_ARROW && playerY > 0) {
-      playerY--;
-    }
-    
-    if (keyPress == DOWN_ARROW && playerY < 192) {
-      playerY++;
-    }
+    switch (keyPress) {
+      case LEFT_ARROW: if (playerX > 0) { playerX--; }
+        break;
+  
+      case RIGHT_ARROW: if (playerX < 156) { playerX++; }
+        break;
 
-    GTEMoveSprite(playerSpriteSlot, playerX, playerY);
+      case UP_ARROW: if (playerY > 0) { playerY--; }
+        break;
+
+      case DOWN_ARROW: if (playerY < 192) { playerY++; }
+        break;
+    }
+ 
+    GTEMoveSprite(SPRITE_SLOT, playerX, playerY);
     GTERender(0);
 
   } while (keyPress != 'q' && keyPress != 'Q');
 
   GTEShutDown();
 
-  ShutDownTools(refIsHandle, toolStartupRef);
-  TOOLFAIL("Unable to shutdown tools");
+  DisposeHandle(dpHndl);
 
-  TLShutDown();
-  TOOLFAIL("Unable to shutdown tool locator");
+  MTShutDown();
+  TOOLFAIL("Unable to shutdown misc tool");
 
   MMShutDown(userId);
   TOOLFAIL("Unable to shutdown memory manager");
+
+  TLShutDown();
+  TOOLFAIL("Unable to shutdown tool locator");
 }
